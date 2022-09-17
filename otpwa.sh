9d40db0d77c41d6c7ed0210af641201241a08a5c
MRONGOS <110918240+mr4605@users.noreply.github.com>
2022-09-17 14:38:56 +0700

    Add files via upload

---
+++ otpwa.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+ 
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 3
+clear
+figlet "MRONGOS"|lolcat
+echo ""
+echo $cyan "Mod by 49035"
+echo $cyan "WA : +6285xxxxx"
+echo $cyan "FB : UsherAcound"
+echo $cyan "Notif : Nothing Give Up "
+echo $yellow "Mung iso di kanggoke x5 nggon IP sing podo"
+sleep 5
+echo $white "SABAR MBAHH ........."
+sleep 10
+echo $cyan " TULISEN JENENG TARGET :"
+read kartu
+echo $cyan " TULISEN NOMER TARGET :"
+read anda
+echo "\033[0;32m]List Socks : "
+read socks
+echo $red "LAGI MIKIR ....."
+sleep 5
+echo $red "SABAR MBAHH......."
+sleep 5
+echo $red "LAGI JEBOL LAWANG $kertu"
+echo $red "system Sukses JEBOL LAWANG 192.168.85.685:9685......"
+sleep 5
+echo $red "Lagi Ngirim OTP Neng $Awakmu"
+sleep 3
+echo $red "NGIRIM DATA ......"
+sleep 5
+echo $red "Socks $pulsa Reading List......"
+sleep 5
+echo "Mbukak...."
+sleep 3
+echo "1%"
+sleep 3
+echo "35%"
+sleep 3
+echo "50%"
+echo "60%"
+sleep 3
+echo "80%"
+sleep 3
+echo "90%"
+sleep 3
+echo "100%"
+sleep 5
+echo "SABAR MBAHH........."
+sleep 1
+echo "Sukses Mbahh......"
+sleep 1
+echo "Sukses Njebol Lawang,  Di Kirim Nek : $Awakmu"
+echo "Kode OTP Wa : 490 355"
+echo "SUWUN 🙏🙏🙏"
